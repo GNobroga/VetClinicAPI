73747942241391ad18b86420bf3d8c4a403e4748
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-03-04 09:17:31 -0300

    feat: DefaulController to redirect Swagger Ui

---
+++ src/main/java/com/veterinary/care/api/application/controllers/DefaultController.java
@@ -0,0 +1,18 @@
+package com.veterinary.care.api.application.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping
+public class DefaultController {
+
+    @GetMapping
+    public ModelAndView redirectToSwagger() {
+        return new ModelAndView("redirect:/swagger-ui.html");
+    }
+
+}
